7539db9b119e984269a7a627c3bdc4517b0a438e
Ferdi Durgun <ferdidurgun34@gmail.com>
2024-09-22 05:46:30 +0300

    Update home_screen.dart

--- lib/presentation/pages/main_pages/home_screen.dart
+++ lib/presentation/pages/main_pages/home_screen.dart
@@ -79,7 +79,6 @@ class _HomeScreenState extends State<HomeScreen> {
         _shows = fetchedShows;
         _stages = fetchedStages;
         _campaigns = fetchedCampaigns;
-
       });
     } catch (e) {
       throw Exception('Veriler getirilemedi: $e');
@@ -88,7 +87,8 @@ class _HomeScreenState extends State<HomeScreen> {
 
   void _navigateToSearch() {
     Navigator.push(
-      context, MaterialPageRoute(builder: (context) => const SearchPage()),
+      context,
+      MaterialPageRoute(builder: (context) => const SearchPage()),
     );
   }
 
@@ -96,20 +96,26 @@ class _HomeScreenState extends State<HomeScreen> {
     if (url.contains('player')) {
       Navigator.push(
         context,
-        MaterialPageRoute(builder: (context) => PlayerDetailPage(playerId: _extractIdFromUrl(url))),
+        MaterialPageRoute(
+            builder: (context) =>
+                PlayerDetailPage(playerId: _extractIdFromUrl(url))),
       );
     } else if (url.contains('stage')) {
       Navigator.push(
         context,
-        MaterialPageRoute(builder: (context) => StageDetailPage(stageId: _extractIdFromUrl(url))),
+        MaterialPageRoute(
+            builder: (context) =>
+                StageDetailPage(stageId: _extractIdFromUrl(url))),
       );
     } else if (url.contains('show')) {
       Navigator.push(
         context,
-        MaterialPageRoute(builder: (context) => ShowDetailPage(showId: _extractIdFromUrl(url))),
+        MaterialPageRoute(
+            builder: (context) =>
+                ShowDetailPage(showId: _extractIdFromUrl(url))),
       );
     } else {
-      print('Unknown URL: $url');
+      throw Exception('Unknown URL: $url');
     }
   }
 
@@ -123,35 +129,30 @@ class _HomeScreenState extends State<HomeScreen> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      body: SingleChildScrollView(
-        padding: const EdgeInsets.all(16),
-        child: Column(
-          children: [
-            _buildCampaignSlider(),
-            CustomSearchBar(onSearchTap: _navigateToSearch),
-            const SizedBox(height: 20),
-            const CustomSectionTitle(title: 'Kategoriler'),
-            const CategoryCardBuilder(),
-            const CustomSectionTitle(title: 'Yeni Gösteriler'),
-            _buildNewShows(),
-            const CustomSectionTitle(title: 'Sahneler'),
-            _buildStageSection(),
-            const CustomSectionTitle(title: 'Oyunlardan Kareler'),
-            _buildGamesPhotoSection(),
-            const SizedBox(height: 50),
-          ],
-        ),
-      ),
-    );
+        body: SingleChildScrollView(
+            padding: const EdgeInsets.all(16),
+            child: Column(children: [
+              _buildCampaignSlider(),
+              CustomSearchBar(onSearchTap: _navigateToSearch),
+              const SizedBox(height: 20),
+              const CustomSectionTitle(title: 'Kategoriler'),
+              const CategoryCardBuilder(),
+              const CustomSectionTitle(title: 'Yeni Gösteriler'),
+              _buildNewShows(),
+              const CustomSectionTitle(title: 'Sahneler'),
+              _buildStageSection(),
+              const CustomSectionTitle(title: 'Oyunlardan Kareler'),
+              _buildGamesPhotoSection(),
+              const SizedBox(height: 50)
+            ])));
   }
 
   Widget _buildCampaignSlider() {
     return Container(
-      padding: const EdgeInsets.all(20.0),
-      width: double.infinity,
-      height: MediaQuery.of(context).size.height * 0.3,
-      child: Column(
-        children: [
+        padding: const EdgeInsets.all(20.0),
+        width: double.infinity,
+        height: MediaQuery.of(context).size.height * 0.3,
+        child: Column(children: [
           Expanded(
             child: PageView.builder(
               controller: _pageController,
@@ -162,167 +163,140 @@ class _HomeScreenState extends State<HomeScreen> {
             ),
           ),
           const SizedBox(height: 10),
-          _buildDotsIndicator(),
-        ],
-      ),
-    );
+          _buildDotsIndicator()
+        ]));
   }
 
   Widget _buildCampaignPage(Campaign? campaign) {
     if (campaign == null) return const SizedBox();
 
     return GestureDetector(
-      onTap: () {
-        _navigateToDetailPage(campaign.url);
-      },
-      child: Card(
-        elevation: 8,
-        shape: RoundedRectangleBorder(
-          borderRadius: BorderRadius.circular(15),
-        ),
-        child: Stack(
-          alignment: Alignment.bottomCenter,
-          children: [
-            CachedNetworkImage(
-              imageUrl: campaign.imageUrl,
-              width: double.infinity,
-              height: double.infinity,
-              fit: BoxFit.cover,
-              placeholder: (context, url) => const CircularProgressIndicator(),
-              errorWidget: (context, url, error) => const Icon(Icons.error),
-            ),
-            Container(
-              color: Colors.black.withOpacity(0.5),
-              padding: const EdgeInsets.all(8),
-              child: Text(
-                campaign.title,
-                style: const TextStyle(fontSize: 24, color: Colors.white),
-                textAlign: TextAlign.center,
-              ),
-            ),
-          ],
-        ),
-      ),
-    );
-  }
-
-  Widget _buildDotsIndicator() {
-    return DotsIndicator(
-      controller: _pageController,
-      itemCount: _campaigns.length,
-      onPageSelected: (page) {
-        setState(() {
-          _currentPage = page;
-        });
-        _pageController.animateToPage(
-          page,
-          duration: const Duration(seconds: 1),
-          curve: Curves.easeInOut,
-        );
-      },
-    );
-  }
-
-  Widget _buildNewShows() {
-    return SizedBox(
-      height: 200,
-      child: ListView.builder(
-        scrollDirection: Axis.horizontal,
-        padding: const EdgeInsets.symmetric(horizontal: 16),
-        itemCount: _shows.length,
-        itemBuilder: (context, index) {
-          return GestureDetector(
-            child: CustomVerticalShowCard(
-              imageUrl: _shows[index]?.imageUrl ?? '',
-              gameName: _shows[index]?.name ?? '',
-              onTap: () {
-                Navigator.push(
-                  context,
-                  MaterialPageRoute(
-                      builder: (context) =>
-                          ShowDetailPage(showId: _shows[index]?.id ?? '')),
-                );
-              },
-            ),
-          );
+        onTap: () {
+          _navigateToDetailPage(campaign.url);
         },
-      ),
-    );
-  }
-
-  Widget _buildStageSection() {
-    return SizedBox(
-      height: 200,
-      child: ListView.builder(
-        scrollDirection: Axis.horizontal,
-        padding: const EdgeInsets.symmetric(horizontal: 16),
-        itemCount: _stages.length,
-        itemBuilder: (context, index) {
-          return CustomStageCard(
-            text: _stages[index]?.name ?? '',
-            imageUrl: _stages[index]?.imageUrl ?? '',
-            onPressed: () {
-              Navigator.push(
-                context,
-                MaterialPageRoute(
-                    builder: (context) =>
-                        StageDetailPage(stageId: _stages[index]?.id ?? '')),
-              );
-            },
-          );
-        },
-      ),
-    );
-  }
-
-  Widget _buildGamesPhotoSection() {
-    return SizedBox(
-      height: 210,
-      child: ListView.builder(
-        scrollDirection: Axis.horizontal,
-        padding: const EdgeInsets.symmetric(horizontal: 16),
-        itemCount: 6,
-        itemBuilder: (context, index) {
-          return _buildGamePhotoCard(index);
-        },
-      ),
-    );
-  }
-
-  Widget _buildGamePhotoCard(int index) {
-    return Container(
-      width: 160,
-      margin: const EdgeInsets.only(right: 16),
-      child: Card(
-        elevation: 8,
-        child: Column(
-          crossAxisAlignment: CrossAxisAlignment.start,
-          children: [
-            Padding(
-              padding: const EdgeInsets.fromLTRB(8, 8, 8, 0),
-              child: CachedNetworkImage(
-                imageUrl:
-                    'https://i.ytimg.com/vi/tzPpkRLf9a8/hq720.jpg?sqp=-oaymwE7CK4FEIIDSFryq4qpAy0IARUAAAAAGAElAADIQj0AgKJD8AEB-AH-CYAC0AWKAgwIABABGHIgWyg9MA8=&rs=AOn4CLCBnYXpB7USjvYDePL64AaVI7Epyw',
-                height: 150,
+        child: Card(
+            elevation: 8,
+            shape: RoundedRectangleBorder(
+              borderRadius: BorderRadius.circular(15),
+            ),
+            child: Stack(alignment: Alignment.bottomCenter, children: [
+              CachedNetworkImage(
+                imageUrl: campaign.imageUrl,
                 width: double.infinity,
+                height: double.infinity,
                 fit: BoxFit.cover,
                 placeholder: (context, url) =>
                     const CircularProgressIndicator(),
                 errorWidget: (context, url, error) => const Icon(Icons.error),
               ),
-            ),
-            const SizedBox(height: 5),
-            Padding(
-              padding: const EdgeInsets.all(8),
-              child: Text(
-                'Oyun $index',
-                style: const TextStyle(fontSize: 16),
+              Container(
+                  color: Colors.black.withOpacity(0.5),
+                  padding: const EdgeInsets.all(8),
+                  child: Text(campaign.title,
+                      style: const TextStyle(fontSize: 24, color: Colors.white),
+                      textAlign: TextAlign.center))
+            ])));
+  }
+
+  Widget _buildDotsIndicator() {
+    return DotsIndicator(
+        controller: _pageController,
+        itemCount: _campaigns.length,
+        onPageSelected: (page) {
+          setState(() {
+            _currentPage = page;
+          });
+          _pageController.animateToPage(
+            page,
+            duration: const Duration(seconds: 1),
+            curve: Curves.easeInOut,
+          );
+        });
+  }
+
+  Widget _buildNewShows() {
+    return SizedBox(
+        height: 200,
+        child: ListView.builder(
+            scrollDirection: Axis.horizontal,
+            padding: const EdgeInsets.symmetric(horizontal: 16),
+            itemCount: _shows.length,
+            itemBuilder: (context, index) {
+              return GestureDetector(
+                  child: CustomVerticalShowCard(
+                      imageUrl: _shows[index]?.imageUrl ?? '',
+                      gameName: _shows[index]?.name ?? '',
+                      onTap: () {
+                        Navigator.push(
+                          context,
+                          MaterialPageRoute(
+                              builder: (context) => ShowDetailPage(
+                                  showId: _shows[index]?.id ?? '')),
+                        );
+                      }));
+            }));
+  }
+
+  Widget _buildStageSection() {
+    return SizedBox(
+        height: 200,
+        child: ListView.builder(
+            scrollDirection: Axis.horizontal,
+            padding: const EdgeInsets.symmetric(horizontal: 16),
+            itemCount: _stages.length,
+            itemBuilder: (context, index) {
+              return CustomStageCard(
+                  text: _stages[index]?.name ?? '',
+                  imageUrl: _stages[index]?.imageUrl ?? '',
+                  onPressed: () {
+                    Navigator.push(
+                        context,
+                        MaterialPageRoute(
+                            builder: (context) => StageDetailPage(
+                                stageId: _stages[index]?.id ?? '')));
+                  });
+            }));
+  }
+
+  Widget _buildGamesPhotoSection() {
+    return SizedBox(
+        height: 210,
+        child: ListView.builder(
+            scrollDirection: Axis.horizontal,
+            padding: const EdgeInsets.symmetric(horizontal: 16),
+            itemCount: 6,
+            itemBuilder: (context, index) {
+              return _buildGamePhotoCard(index);
+            }));
+  }
+
+  Widget _buildGamePhotoCard(int index) {
+    return Container(
+        width: 160,
+        margin: const EdgeInsets.only(right: 16),
+        child: Card(
+            elevation: 8,
+            child:
+                Column(crossAxisAlignment: CrossAxisAlignment.start, children: [
+              Padding(
+                padding: const EdgeInsets.fromLTRB(8, 8, 8, 0),
+                child: CachedNetworkImage(
+                  imageUrl:
+                      'https://i.ytimg.com/vi/tzPpkRLf9a8/hq720.jpg?sqp=-oaymwE7CK4FEIIDSFryq4qpAy0IARUAAAAAGAElAADIQj0AgKJD8AEB-AH-CYAC0AWKAgwIABABGHIgWyg9MA8=&rs=AOn4CLCBnYXpB7USjvYDePL64AaVI7Epyw',
+                  height: 150,
+                  width: double.infinity,
+                  fit: BoxFit.cover,
+                  placeholder: (context, url) =>
+                      const CircularProgressIndicator(),
+                  errorWidget: (context, url, error) => const Icon(Icons.error),
+                ),
               ),
-            ),
-          ],
-        ),
-      ),
-    );
+              const SizedBox(height: 5),
+              Padding(
+                  padding: const EdgeInsets.all(8),
+                  child:
+                      Text('Oyun $index', style: const TextStyle(fontSize: 16)))
+            ])));
   }
 }
 
@@ -341,25 +315,23 @@ class DotsIndicator extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Row(
-      mainAxisAlignment: MainAxisAlignment.center,
-      children: List.generate(itemCount, (index) {
-        return _buildDot(index);
-      }),
-    );
+        mainAxisAlignment: MainAxisAlignment.center,
+        children: List.generate(itemCount, (index) {
+          return _buildDot(index);
+        }));
   }
 
   Widget _buildDot(int index) {
     return GestureDetector(
-      onTap: () => onPageSelected(index),
-      child: Container(
-        margin: const EdgeInsets.symmetric(horizontal: 4.0),
-        width: 8.0,
-        height: 8.0,
-        decoration: BoxDecoration(
-          shape: BoxShape.circle,
-          color: controller.page?.round() == index ? Colors.red : Colors.grey,
-        ),
-      ),
-    );
+        onTap: () => onPageSelected(index),
+        child: Container(
+            margin: const EdgeInsets.symmetric(horizontal: 4.0),
+            width: 8.0,
+            height: 8.0,
+            decoration: BoxDecoration(
+                shape: BoxShape.circle,
+                color: controller.page?.round() == index
+                    ? Colors.red
+                    : Colors.grey)));
   }
 }
