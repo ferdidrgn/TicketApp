f4d42622795340f8fdc95f8a252901757ae21105
Ferdi Durgun <ferdidurgun34@gmail.com>
2025-03-15 10:33:47 +0300

    Update contracts.dart

--- lib/presentation/pages/contracts/contracts.dart
+++ lib/presentation/pages/contracts/contracts.dart
@@ -1,135 +1,132 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_riverpod/flutter_riverpod.dart';
 import 'package:simple_html_css/simple_html_css.dart';
-import '../../../data/repository/app_tools_service.dart';
+import '../../../data/providers/appTools/app_tools_provider.dart';
 
-class ContractsPage extends StatefulWidget {
+class ContractsPage extends ConsumerWidget {
   const ContractsPage({super.key});
 
   @override
-  _ContractsPageState createState() => _ContractsPageState();
-}
+  Widget build(final BuildContext context, final WidgetRef ref) {
+    final appToolsState = ref.watch(appToolsProvider);
 
-class _ContractsPageState extends State<ContractsPage> {
-  String? termsAndConditionsHtml;
-  String? privacyPolicyHtml;
-  final AppToolsService appToolsService = AppToolsService();
-
-  @override
-  void initState() {
-    super.initState();
-    fetchHtmlContent();
-  }
-
-  Future<void> fetchHtmlContent() async {
-    final privacy = await appToolsService.getPrivacyPolicy();
-    final terms = await appToolsService.getTermsCondition();
-    setState(() {
-      privacyPolicyHtml = privacy;
-      termsAndConditionsHtml = terms;
-    });
-  }
-
-  @override
-  Widget build(final BuildContext context) {
     return Scaffold(
       appBar: AppBar(
         title: const Text('Privacy Policy & Terms & Conditions'),
         centerTitle: true,
       ),
-      body: Padding(
-        padding: const EdgeInsets.all(20),
-        child: Column(
-          children: [
-            Expanded(
-              child: Column(
-                crossAxisAlignment: CrossAxisAlignment.start,
-                children: [
-                  Align(
-                    alignment: Alignment.topLeft,
-                    child: Card(
-                      elevation: 3,
-                      shape: RoundedRectangleBorder(
-                        borderRadius: BorderRadius.circular(8),
-                      ),
-                      child: Container(
-                        width: double.infinity, // Makes the card full-width
-                        padding: const EdgeInsets.all(16.0),
-                        child: const Text(
-                          'Privacy Policy',
-                          style: TextStyle(
-                            fontSize: 18,
-                            fontWeight: FontWeight.bold,
+      body: appToolsState.isLoading
+          ? const Center(child: CircularProgressIndicator())
+          : appToolsState.errorMessage != null
+              ? _buildErrorState(appToolsState.errorMessage!)
+              : _buildContentState(context, appToolsState.privacyPolicy,
+                  appToolsState.termsCondition),
+    );
+  }
+
+  Widget _buildErrorState(final String message) {
+    return Center(child: Text(message));
+  }
+
+  Widget _buildContentState(final BuildContext context,
+      final String? privacyPolicy, final String? termsCondition) {
+    return Scaffold(
+      body: SingleChildScrollView(
+        child: Padding(
+          padding: const EdgeInsets.all(20),
+          child: Column(
+            children: [
+              Expanded(
+                child: Column(
+                  crossAxisAlignment: CrossAxisAlignment.start,
+                  children: [
+                    Align(
+                      alignment: Alignment.topLeft,
+                      child: Card(
+                        elevation: 3,
+                        shape: RoundedRectangleBorder(
+                          borderRadius: BorderRadius.circular(8),
+                        ),
+                        child: Container(
+                          width: double.infinity,
+                          padding: const EdgeInsets.all(16.0),
+                          child: const Text(
+                            'Privacy Policy',
+                            style: TextStyle(
+                              fontSize: 18,
+                              fontWeight: FontWeight.bold,
+                            ),
                           ),
                         ),
                       ),
                     ),
-                  ),
-                  const SizedBox(height: 10),
-                  Expanded(
-                    child: SingleChildScrollView(
-                      child: privacyPolicyHtml != null
-                          ? RichText(
-                        text: HTML.toTextSpan(
-                          context,
-                          privacyPolicyHtml!,
-                          defaultTextStyle: const TextStyle(
-                            fontSize: 18,
-                            decoration: TextDecoration.none,
-                          ),
-                        ),
-                      )
-                          : const Center(child: CircularProgressIndicator()),
+                    const SizedBox(height: 10),
+                    Expanded(
+                      child: SingleChildScrollView(
+                        child: privacyPolicy != null
+                            ? RichText(
+                                text: HTML.toTextSpan(
+                                  context,
+                                  privacyPolicy,
+                                  defaultTextStyle: const TextStyle(
+                                    fontSize: 18,
+                                    decoration: TextDecoration.none,
+                                  ),
+                                ),
+                              )
+                            : const Center(child: CircularProgressIndicator()),
+                      ),
                     ),
-                  ),
-                ],
+                  ],
+                ),
               ),
-            ),
-            const Divider(height: 2, thickness: 1),
-            Expanded(
-              child: Column(
-                crossAxisAlignment: CrossAxisAlignment.start,
-                children: [
-                  Align(
-                    alignment: Alignment.topLeft,
-                    child: Card(
-                      elevation: 3,
-                      shape: RoundedRectangleBorder(
-                        borderRadius: BorderRadius.circular(8),
-                      ),
-                      child: Container(
-                        width: double.infinity, // Makes the card full-width
-                        padding: const EdgeInsets.all(16.0),
-                        child: const Text(
-                          'Terms And Conditions',
-                          style: TextStyle(
-                            fontSize: 18,
-                            fontWeight: FontWeight.bold,
+              const Divider(height: 2, thickness: 1),
+              Expanded(
+                child: Column(
+                  crossAxisAlignment: CrossAxisAlignment.start,
+                  children: [
+                    Align(
+                      alignment: Alignment.topLeft,
+                      child: Card(
+                        elevation: 3,
+                        shape: RoundedRectangleBorder(
+                          borderRadius: BorderRadius.circular(8),
+                        ),
+                        child: Container(
+                          width: double.infinity,
+                          padding: const EdgeInsets.all(16.0),
+                          child: const Text(
+                            'Terms And Conditions',
+                            style: TextStyle(
+                              fontSize: 18,
+                              fontWeight: FontWeight.bold,
+                            ),
                           ),
                         ),
                       ),
                     ),
-                  ),
-                  const SizedBox(height: 10),
-                  Expanded(
-                    child: SingleChildScrollView(
-                      child: termsAndConditionsHtml != null
-                          ? RichText(
-                        text: HTML.toTextSpan(
-                          context,
-                          termsAndConditionsHtml!,
-                          defaultTextStyle: const TextStyle(
-                            fontSize: 18,
-                            decoration: TextDecoration.none,
-                          ),
-                        ),
-                      )
-                          : const Center(child: CircularProgressIndicator()),
+                    const SizedBox(height: 10),
+                    Expanded(
+                      child: SingleChildScrollView(
+                        child: termsCondition != null
+                            ? RichText(
+                                text: HTML.toTextSpan(
+                                  context,
+                                  termsCondition,
+                                  defaultTextStyle: const TextStyle(
+                                    fontSize: 18,
+                                    decoration: TextDecoration.none,
+                                  ),
+                                ),
+                              )
+                            : const Center(child: CircularProgressIndicator()),
+                      ),
                     ),
-                  ),
-                ],
+                  ],
+                ),
               ),
-            ),
-          ],
+            ],
+          ),
         ),
       ),
     );
