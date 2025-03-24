9b3a8ca8f3561d9b6aad89a51d664efbfc211364
Ferdi Durgun <ferdidurgun34@gmail.com>
2025-03-25 01:40:14 +0300

    Update campaign_notifier.dart

--- lib/data/providers/campaign/campaign_notifier.dart
+++ lib/data/providers/campaign/campaign_notifier.dart
@@ -1,38 +1,18 @@
-import 'package:flutter_riverpod/flutter_riverpod.dart';
+import 'package:ticketapp/core/common/base_notifier.dart';
+
 import '../../../domain/useCase/campaign/get_campaigns_use_case_impl.dart';
-import '../../model/campaing_model.dart';
 import 'campaign_state.dart';
 
-class CampaignNotifier extends StateNotifier<CampaignState> {
+class CampaignNotifier extends BaseNotifier<CampaignState> {
   final GetCampaignsUseCase getCampaignsUseCase;
 
   CampaignNotifier(this.getCampaignsUseCase) : super(CampaignState());
 
   Future<void> loadCampaigns() async {
-    _setLoadingState(true);
-
-    try {
-      final result = await getCampaignsUseCase.call();
-      result.fold(
-        (final failure) => _setErrorState(failure.message),
-        (final campaignList) => _setCampaignsState(campaignList ?? []),
-      );
-    } catch (e) {
-      _setErrorState("Beklenmeyen bir hata oluştu");
-    } finally {
-      _setLoadingState(false);
-    }
-  }
-
-  void _setCampaignsState(final List<CampaignModel?> campaigns) {
-    state = state.copyWith(campaigns: campaigns, errorMessage: null);
-  }
-
-  void _setErrorState(final String message) {
-    state = state.copyWith(errorMessage: message);
-  }
-
-  void _setLoadingState(final isLoading) {
-    state = state.copyWith(isLoading: isLoading);
+    await handleOperation(
+      () => getCampaignsUseCase.call(),
+      onSuccess: (final campaigns) =>
+          state = state.copyWith(campaigns: campaigns),
+    );
   }
 }
