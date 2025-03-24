232ffb853fdb33e573a495d9712fb185af1b9983
Ferdi Durgun <ferdidurgun34@gmail.com>
2025-03-25 02:13:17 +0300

    Update base_repo.dart

--- lib/core/common/base_repo.dart
+++ lib/core/common/base_repo.dart
@@ -7,16 +7,23 @@ abstract class BaseRepository {
 
   BaseRepository({required this.internetService});
 
-  Future<Either<Failure, T>> execute<T>(final Future<T> Function() action) async {
+  Future<Either<Failure, T>> execute<T>(
+      final Future<T> Function() action) async {
     if (await internetService.isConnected) {
       try {
         final result = await action();
-        if ((result is List && result.isEmpty) || result == null)
-          return const Left(ServerFailure('No data found'));
+
+        if ((result is List && result.isEmpty) || result == null) {
+          Left(ServerFailure('No data found')); // Hata döndür
+          return Right([] as T); // Boş liste döndür
+        }
+
         return Right(result);
       } catch (e) {
         return Left(ServerFailure(e.toString()));
       }
-    } else return const Left(NetworkFailure('No internet connection'));
+    } else {
+      return const Left(NetworkFailure('No internet connection'));
+    }
   }
 }
