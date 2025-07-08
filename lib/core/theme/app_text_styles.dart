17d5b446da4ab08ed9ead319519abe10ddcc5fa7
ferdidrgn <ferdidurgun34@gmail.com>
2025-07-08 06:20:00 +0300

    update text

--- lib/core/theme/app_text_styles.dart
+++ lib/core/theme/app_text_styles.dart
@@ -1,7 +1,7 @@
 import 'package:flutter/material.dart';
 
 mixin AppTextStyles {
-  static const String fontFamily = 'Roboto';
+  static const String fontFamily = 'PlayfairDisplay';
 
   static const baseLightTextStyle =
       TextStyle(fontFamily: fontFamily, color: Colors.black);
@@ -15,25 +15,25 @@ mixin AppTextStyles {
     displayMedium: baseLightTextStyle.copyWith(
         fontSize: 60.0, fontWeight: FontWeight.w300),
     displaySmall: baseLightTextStyle.copyWith(
-        fontSize: 48.0, fontWeight: FontWeight.w400),
+        fontSize: 44.0, fontWeight: FontWeight.w400),
     headlineLarge: baseLightTextStyle.copyWith(
-        fontSize: 34.0, fontWeight: FontWeight.w400),
+        fontSize: 30.0, fontWeight: FontWeight.w400),
     headlineMedium: baseLightTextStyle.copyWith(
         fontSize: 24.0, fontWeight: FontWeight.w400),
     headlineSmall: baseLightTextStyle.copyWith(
         fontSize: 20.0, fontWeight: FontWeight.w500),
     titleLarge: baseLightTextStyle.copyWith(
-        fontSize: 16.0, fontWeight: FontWeight.w400),
+        fontSize: 18.0, fontWeight: FontWeight.w400),
     titleMedium: baseLightTextStyle.copyWith(
-        fontSize: 14.0, fontWeight: FontWeight.w500),
+        fontSize: 16.0, fontWeight: FontWeight.w500),
     titleSmall: baseLightTextStyle.copyWith(
-        fontSize: 12.0, fontWeight: FontWeight.w400),
+        fontSize: 14.0, fontWeight: FontWeight.w400),
     bodyLarge: baseLightTextStyle.copyWith(
         fontSize: 16.0, fontWeight: FontWeight.w400),
     bodyMedium: baseLightTextStyle.copyWith(
         fontSize: 14.0, fontWeight: FontWeight.w400),
     bodySmall: baseLightTextStyle.copyWith(
-        fontSize: 12.0, fontWeight: FontWeight.w400),
+        fontSize: 13.0, fontWeight: FontWeight.w400),
     labelLarge: baseLightTextStyle.copyWith(
         fontSize: 14.0, fontWeight: FontWeight.w500),
     labelMedium: baseLightTextStyle.copyWith(
@@ -48,25 +48,25 @@ mixin AppTextStyles {
     displayMedium: baseDarkTextStyle.copyWith(
         fontSize: 60.0, fontWeight: FontWeight.w300),
     displaySmall: baseDarkTextStyle.copyWith(
-        fontSize: 48.0, fontWeight: FontWeight.w400),
+        fontSize: 44.0, fontWeight: FontWeight.w400),
     headlineLarge: baseDarkTextStyle.copyWith(
-        fontSize: 34.0, fontWeight: FontWeight.w400),
+        fontSize: 30.0, fontWeight: FontWeight.w400),
     headlineMedium: baseDarkTextStyle.copyWith(
         fontSize: 24.0, fontWeight: FontWeight.w400),
     headlineSmall: baseDarkTextStyle.copyWith(
         fontSize: 20.0, fontWeight: FontWeight.w500),
     titleLarge: baseDarkTextStyle.copyWith(
-        fontSize: 16.0, fontWeight: FontWeight.w400),
+        fontSize: 18.0, fontWeight: FontWeight.w400),
     titleMedium: baseDarkTextStyle.copyWith(
-        fontSize: 14.0, fontWeight: FontWeight.w500),
+        fontSize: 16.0, fontWeight: FontWeight.w500),
     titleSmall: baseDarkTextStyle.copyWith(
-        fontSize: 12.0, fontWeight: FontWeight.w400),
+        fontSize: 14.0, fontWeight: FontWeight.w400),
     bodyLarge: baseDarkTextStyle.copyWith(
         fontSize: 16.0, fontWeight: FontWeight.w400),
     bodyMedium: baseDarkTextStyle.copyWith(
         fontSize: 14.0, fontWeight: FontWeight.w400),
     bodySmall: baseDarkTextStyle.copyWith(
-        fontSize: 12.0, fontWeight: FontWeight.w400),
+        fontSize: 13.0, fontWeight: FontWeight.w400),
     labelLarge: baseDarkTextStyle.copyWith(
         fontSize: 14.0, fontWeight: FontWeight.w500),
     labelMedium: baseDarkTextStyle.copyWith(
