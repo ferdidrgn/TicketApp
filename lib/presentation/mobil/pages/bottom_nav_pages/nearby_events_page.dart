180757399bc482369e7e14b3bbbdb0c114c61465
ferdidrgn <ferdidurgun34@gmail.com>
2025-07-08 04:53:31 +0300

    Update nearby_events_page.dart

--- lib/presentation/mobil/pages/bottom_nav_pages/nearby_events_page.dart
+++ lib/presentation/mobil/pages/bottom_nav_pages/nearby_events_page.dart
@@ -1,79 +1,70 @@
 import 'package:flutter/material.dart';
 import '../../../../core/widgets/custom_event_card.dart';
+import '../../../../core/widgets/custom_title.dart';
 
 class NearbyEventsPage extends StatefulWidget {
   const NearbyEventsPage({super.key});
 
   @override
-  _NearbyEventsPageState createState() => _NearbyEventsPageState();
+  State<NearbyEventsPage> createState() => _NearbyEventsPageState();
 }
 
 class _NearbyEventsPageState extends State<NearbyEventsPage> {
-  final List<String> _categories = [
-    'Müzikal',
-    'Tiyatro',
-    'Sinema',
-    'Dans',
-    'Opera',
-    'Bale'
-  ];
-  final List<String> _selectedCategories = [];
-  RangeValues _priceRange = const RangeValues(0, 1000);
+  final categories = ['Müzikal', 'Tiyatro', 'Sinema', 'Dans', 'Opera', 'Bale'];
+  final selectedCategories = <String>[];
+  RangeValues priceRange = const RangeValues(0, 1000);
 
   void _showFilterDialog() {
     showDialog(
       context: context,
-      builder: (final BuildContext context) {
-        return AlertDialog(
-          title: const Text('Filtreler'),
-          content: SingleChildScrollView(
-            child: Column(
-              crossAxisAlignment: CrossAxisAlignment.start,
-              children: [
-                const Text('Kategoriler:'),
-                ..._categories.map((final category) => CheckboxListTile(
-                      title: Text(category),
-                      value: _selectedCategories.contains(category),
-                      onChanged: (final bool? checked) {
-                        setState(() {
-                          if (checked != null && checked == true) {
-                            _selectedCategories.add(category);
-                          } else {
-                            _selectedCategories.remove(category);
-                          }
-                        });
-                      },
-                    )),
-                const SizedBox(height: 20),
-                const Text('Fiyat Aralığı:'),
-                RangeSlider(
-                  values: _priceRange,
-                  min: 0,
-                  max: 1000,
-                  divisions: 10,
-                  labels: RangeLabels(
-                    '${_priceRange.start.round()}₺',
-                    '${_priceRange.end.round()}₺',
-                  ),
-                  onChanged: (final RangeValues values) {
-                    setState(() {
-                      _priceRange = values;
-                    });
-                  },
+      builder: (final _) => AlertDialog(
+        title: Text('Filtreler',
+            style: Theme.of(context).textTheme.headlineMedium),
+        content: SingleChildScrollView(
+          child: Column(
+            crossAxisAlignment: CrossAxisAlignment.start,
+            children: [
+              const CustomSectionTitle(title: 'Kategoriler:'),
+              ...categories.map(
+                (final cat) => CheckboxListTile(
+                  title:
+                      Text(cat, style: Theme.of(context).textTheme.bodyMedium),
+                  value: selectedCategories.contains(cat),
+                  onChanged: (final checked) => setState(() {
+                    if (checked == true)
+                      selectedCategories.add(cat);
+                    else
+                      selectedCategories.remove(cat);
+                  }),
+                  controlAffinity: ListTileControlAffinity.leading,
+                  contentPadding: EdgeInsets.zero,
                 ),
-              ],
-            ),
+              ),
+              const SizedBox(height: 20),
+              Text('Fiyat Aralığı:',
+                  style: Theme.of(context).textTheme.bodyMedium),
+              RangeSlider(
+                values: priceRange,
+                min: 0,
+                max: 1000,
+                divisions: 10,
+                labels: RangeLabels(
+                  '${priceRange.start.round()}₺',
+                  '${priceRange.end.round()}₺',
+                ),
+                onChanged: (final v) => setState(() => priceRange = v),
+              ),
+            ],
           ),
-          actions: [
-            TextButton(
-              onPressed: () {
-                Navigator.of(context).pop();
-              },
-              child: const Text('Uygula'),
-            ),
-          ],
-        );
-      },
+        ),
+        actions: [
+          TextButton(
+            onPressed: () => Navigator.pop(context),
+            child:
+                Text('Uygula', style: Theme.of(context).textTheme.bodyMedium),
+          ),
+        ],
+      ),
     );
   }
 
@@ -81,62 +72,58 @@ class _NearbyEventsPageState extends State<NearbyEventsPage> {
   Widget build(final BuildContext context) {
     return Scaffold(
       body: Padding(
-        padding: const EdgeInsets.all(16.0),
-        child: Column(
-          crossAxisAlignment: CrossAxisAlignment.start,
-          children: [
-            Row(
-              mainAxisAlignment: MainAxisAlignment.spaceBetween,
-              children: [
-                const Text(
-                  'Yakınınızdaki Etkinlikler',
-                  style: TextStyle(fontSize: 20, fontWeight: FontWeight.bold),
+        padding: const EdgeInsets.all(16),
+        child: Column(crossAxisAlignment: CrossAxisAlignment.start, children: [
+          Row(mainAxisAlignment: MainAxisAlignment.spaceBetween, children: [
+            Text(
+              'Yakınınızdaki Etkinlikler',
+              style: Theme.of(context)
+                  .textTheme
+                  .headlineMedium
+                  ?.copyWith(fontWeight: FontWeight.bold),
+            ),
+            IconButton(
+                icon: const Icon(Icons.filter_list),
+                onPressed: _showFilterDialog),
+          ]),
+          const SizedBox(height: 16),
+          Expanded(
+            child: ListView(
+              children: const [
+                EventCard(
+                  imageUrl:
+                      'https://www.cumhuriyet.com.tr/Archive/2021/8/27/1863857/kapak_002553.jpg',
+                  showName: 'Cimri',
+                  category: 'Müzikal',
+                  date: '12 Eylül 2024',
+                  stage: 'Harbiye',
+                  price: 150,
                 ),
-                IconButton(
-                  icon: const Icon(Icons.filter_list),
-                  onPressed: _showFilterDialog,
+                SizedBox(height: 16),
+                EventCard(
+                  imageUrl:
+                      'https://versustiyatro.com/wp-content/uploads/2016/02/GHT_36101.jpg',
+                  showName: 'Hamlet',
+                  category: 'Tiyatro',
+                  date: '15 Eylül 2024',
+                  stage: 'Zoru',
+                  price: 100,
                 ),
+                SizedBox(height: 16),
+                EventCard(
+                  imageUrl:
+                      'https://tiyatronline.com/isDosyalar/2019/05/20/crop_gozlerimi-kaparim-vazifemi-yaparim-ank_ilf4LaFHkp.jpg',
+                  showName: 'Gözlerimi Kaparım Vazifemi Yaparım',
+                  category: 'Sinema',
+                  date: '20 Eylül 2024',
+                  stage: 'Göztepe',
+                  price: 80,
+                ),
+                SizedBox(height: 50),
               ],
             ),
-            const SizedBox(height: 16),
-            Expanded(
-              child: ListView(
-                children: const [
-                  EventCard(
-                    imageUrl:
-                        'https://www.cumhuriyet.com.tr/Archive/2021/8/27/1863857/kapak_002553.jpg',
-                    showName: 'Cimri',
-                    category: 'Müzikal',
-                    date: '12 Eylül 2024',
-                    stage: 'Harbiye',
-                    price: 150.0,
-                  ),
-                  SizedBox(height: 16),
-                  EventCard(
-                    imageUrl:
-                        'https://versustiyatro.com/wp-content/uploads/2016/02/GHT_36101.jpg',
-                    showName: 'Hamlet',
-                    category: 'Tiyatro',
-                    date: '15 Eylül 2024',
-                    stage: 'Zoru',
-                    price: 100.0,
-                  ),
-                  SizedBox(height: 16),
-                  EventCard(
-                    imageUrl:
-                        'https://tiyatronline.com/isDosyalar/2019/05/20/crop_gozlerimi-kaparim-vazifemi-yaparim-ank_ilf4LaFHkp.jpg',
-                    showName: 'Gözlerimi Kaparım Vazifemi Yaparım',
-                    category: 'Sinema',
-                    date: '20 Eylül 2024',
-                    stage: 'Göztepe',
-                    price: 80.0,
-                  ),
-                  SizedBox(height: 50),
-                ],
-              ),
-            ),
-          ],
-        ),
+          ),
+        ]),
       ),
     );
   }
