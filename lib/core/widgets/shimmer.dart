5eef913d8f12df6c267ab45af72d3d2a7b8cb1d2
Ferdi Durgun <ferdidurgun34@gmail.com>
2025-10-25 12:01:22 +0300

    Update shimmer.dart

--- lib/core/widgets/shimmer.dart
+++ lib/core/widgets/shimmer.dart
@@ -2,18 +2,28 @@ import 'package:flutter/material.dart';
 import 'package:shimmer/shimmer.dart';
 
 class ShimmerLoading extends StatelessWidget {
-  const ShimmerLoading({super.key});
+  final double height;
+  final double width;
+
+  const ShimmerLoading({
+    super.key,
+    this.height = 190.0, // Varsayılan yükseklik
+    this.width = 130.0, // Varsayılan genişlik
+  });
 
   @override
   Widget build(final BuildContext context) {
     return Shimmer.fromColors(
-      baseColor: Theme.of(context).primaryColor,
-      highlightColor: Colors.black,
+      baseColor: Theme.of(context)
+          .primaryColor
+          .withOpacity(0.6), // Ana rengin biraz soluk hali
+      highlightColor: Colors.grey[500]!, // Vurgu için gri tonu
+
       child: Container(
-        width: 130,
-        height: 190,
+        width: width,
+        height: height,
         decoration: BoxDecoration(
-          color: Colors.white,
+          color: Colors.white, // Veya Colors.grey[300]
           borderRadius: BorderRadius.circular(8),
         ),
       ),
