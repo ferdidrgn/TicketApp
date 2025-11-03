190a1ff9e7838679a14d64368d57a64fe7b49b35
Ferdi Durgun <ferdidurgun34@gmail.com>
2025-11-03 13:25:41 +0300

    Update login_state.dart

--- lib/data/providers/login/login_state.dart
+++ lib/data/providers/login/login_state.dart
@@ -3,12 +3,20 @@ import 'package:google_sign_in/google_sign_in.dart';
 import 'package:ticketapp/core/common/base_state.dart';
 
 class LoginState extends BaseState {
-  User? user;
-  GoogleSignInAccount? googleUser;
+  final User? user;
+  final GoogleSignInAccount? googleUser;
+  final String? verificationId;
+  final String? phoneNumber;
+  final bool isCodeSent;
+  final bool isGuest;
 
   LoginState({
     this.user,
     this.googleUser,
+    this.verificationId,
+    this.phoneNumber,
+    this.isCodeSent = false,
+    this.isGuest = false,
     super.isLoading = false,
     super.errorMessage,
   });
@@ -17,14 +25,25 @@ class LoginState extends BaseState {
   LoginState copyWith({
     final User? user,
     final GoogleSignInAccount? googleUser,
+    final String? verificationId,
+    final String? phoneNumber,
+    final bool? isCodeSent,
+    final bool? isGuest,
     final bool? isLoading,
     final String? errorMessage,
-  }) {
-    return LoginState(
-      user: user ?? this.user,
-      googleUser: googleUser ?? this.googleUser,
-      isLoading: isLoading ?? this.isLoading,
-      errorMessage: errorMessage,
-    );
-  }
+  }) =>
+      LoginState(
+        user: user ?? this.user,
+        googleUser: googleUser ?? this.googleUser,
+        verificationId: verificationId ?? this.verificationId,
+        phoneNumber: phoneNumber ?? this.phoneNumber,
+        isCodeSent: isCodeSent ?? this.isCodeSent,
+        isGuest: isGuest ?? this.isGuest,
+        isLoading: isLoading ?? this.isLoading,
+        errorMessage: errorMessage ?? this.errorMessage,
+      );
+
+  bool get isLoggedIn => user != null;
+  bool get isGoogleSignedIn => googleUser != null;
+  bool get isPhoneVerified => phoneNumber != null;
 }
