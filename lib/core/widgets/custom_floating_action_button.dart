84e0d9acb7eb42fb639c5308ce98fa7096633c04
Ferdi Durgun <ferdidurgun34@gmail.com>
2025-11-03 13:05:30 +0300

    Update custom_floating_action_button.dart

--- lib/core/widgets/custom_floating_action_button.dart
+++ lib/core/widgets/custom_floating_action_button.dart
@@ -3,17 +3,13 @@ import 'package:flutter/material.dart';
 class CustomFloatingActionButton extends StatelessWidget {
   final VoidCallback onPressed;
 
-  const CustomFloatingActionButton({
-    super.key,
-    required this.onPressed,
-  });
+  const CustomFloatingActionButton({super.key, required this.onPressed});
 
   @override
   Widget build(final BuildContext context) => Container(
-        margin: EdgeInsets.symmetric(vertical: 40),
-        child: FloatingActionButton(
-          onPressed: onPressed,
-          child: const Icon(Icons.refresh),
-        ),
-      );
+      margin: EdgeInsets.symmetric(vertical: 40),
+      child: FloatingActionButton(
+        onPressed: onPressed,
+        child: const Icon(Icons.refresh),
+      ));
 }
