8719913a600f7ab7a274f6b2ae37eb8a222cd2e9
Ferdi Durgun <ferdidurgun34@gmail.com>
2025-11-28 06:45:15 +0300

    Delete animationsReadyProvider.dart

--- lib/presentation/web/pages/nav_pages/home/widgets/animationsReadyProvider.dart
+++
@@ -1,5 +0,0 @@
-// lib/core/providers/animations_provider.dart
-import 'package:flutter_riverpod/flutter_riverpod.dart';
-import 'package:flutter_riverpod/legacy.dart';
-
-final animationsReadyProvider = StateProvider<bool>((ref) => false);
