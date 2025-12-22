f4e7f409a9b01e566c7721e6d82c76f62025b420
Ferdi Durgun <ferdidurgun34@gmail.com>
2025-12-22 11:05:57 +0300

    Update navigation_button.dart

--- lib/shared/navigation/widgets/navigation_button.dart
+++ lib/shared/navigation/widgets/navigation_button.dart
@@ -7,26 +7,22 @@ class NavigationButton extends StatelessWidget {
   const NavigationButton({super.key, this.onTap});
 
   @override
-  Widget build(final BuildContext context) {
-    if (onTap == null) return const SizedBox.shrink();
-
-    return GestureDetector(
-      onTap: onTap,
-      child: Container(
-        padding: const EdgeInsets.all(8),
-        decoration: BoxDecoration(
-          shape: BoxShape.circle,
-          border: Border.all(
-            color: Colors.grey.withOpacity(0.3),
+  Widget build(final BuildContext context) => GestureDetector(
+        onTap: onTap,
+        child: Container(
+          padding: const EdgeInsets.all(8),
+          decoration: BoxDecoration(
+            shape: BoxShape.circle,
+            border: Border.all(
+              color: Colors.grey.withOpacity(0.3),
+            ),
+            color: context.primaryColor.withOpacity(0.1),
           ),
-          color: context.primaryColor.withOpacity(0.1),
+          child: Icon(
+            Icons.arrow_forward,
+            size: 16,
+            color: context.primaryColor,
+          ),
         ),
-        child: Icon(
-          Icons.arrow_forward,
-          size: 16,
-          color: context.primaryColor,
-        ),
-      ),
-    );
-  }
+      );
 }
