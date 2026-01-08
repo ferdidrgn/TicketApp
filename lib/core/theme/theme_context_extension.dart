8ebb585a42779bf1af365eef9b58a8df108c1e10
Ferdi Durgun <ferdidurgun34@gmail.com>
2026-01-08 07:31:04 +0300

    Update theme_context_extension.dart

--- lib/core/theme/theme_context_extension.dart
+++ lib/core/theme/theme_context_extension.dart
@@ -11,28 +11,40 @@ extension ThemeContextExtension on BuildContext {
   bool get isDarkMode => theme.brightness == Brightness.dark;
 
   // --- Sık Kullanılan Renkler (Kısayollar) ---
-  Color get primaryColor => colors.primary;
-
-  Color get onPrimaryColor => colors.onPrimary;
-
-  Color get secondaryColor => colors.secondary;
-
-  Color get onSecondaryColor => colors.onSecondary;
-
   Color get scaffoldBackgroundColor => theme.scaffoldBackgroundColor;
 
-  Color get surfaceColor => colors.surface;
+  Color get primaryColor => theme.primaryColor;
 
-  Color get errorColor => colors.error;
+  Color get primaryColorLight => theme.primaryColorLight;
 
-  Color get textColor => colors.onSecondary;
+  Color get primaryColorDark => theme.primaryColorDark;
+
+  Color get secondaryHeaderColor => theme.secondaryHeaderColor;
+
+  Color get cardColor => theme.cardColor;
+
+  Color get canvasColor => theme.canvasColor;
+
+  Color get shadowColor => theme.shadowColor;
+
+  Color get dividerColor => theme.dividerColor;
+
+  Color get splashColor => theme.splashColor;
+
+  Color get focusColor => theme.focusColor;
+
+  Color get hintColor => theme.hintColor;
+
+  Color get highlightColor => theme.highlightColor;
+
+  BottomNavigationBarThemeData get bottomNavigationBarTheme =>
+      theme.bottomNavigationBarTheme;
 
   // --- Özel Gradient Mantığın ---
   /// [isActive]: Eğer true ise temanın aktif renklerini (Kırmızı veya Mor),
   /// false ise pasif gri renkleri döndürür.
   List<Color> appGradient({final bool isActive = true}) {
-    if (!isActive)
-      return [Colors.grey[500]!, Colors.grey[800]!];
+    if (!isActive) return [Colors.grey[500]!, Colors.grey[800]!];
 
     return isDarkMode
         ? [Colors.pink[500]!, Colors.purple[600]!] // Dark Mode Gradient
