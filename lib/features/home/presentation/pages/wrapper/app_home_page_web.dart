397a9c7e902b0c123f35cfe3541e4825b7995145
Ferdi Durgun <ferdidurgun34@gmail.com>
2026-01-24 10:45:51 +0300

    Update app_home_page_web.dart

--- lib/features/home/presentation/pages/wrapper/app_home_page_web.dart
+++ lib/features/home/presentation/pages/wrapper/app_home_page_web.dart
@@ -3,6 +3,8 @@ import 'package:flutter_riverpod/flutter_riverpod.dart';
 import 'package:ticketapp/shared/navigation/widgets/web_nav_bar.dart';
 import '../../../../../shared/navigation/providers/navigation_keys.dart';
 import '../../../../../shared/widgets/background/shimmer_components.dart';
+import '../../../../../shared/widgets/global_error_widget.dart';
+import '../../../../shows/presentation/providers/show_provider.dart';
 import '../../providers/home_asset_video_provider.dart';
 
 class AppHomePage extends ConsumerStatefulWidget {
@@ -15,46 +17,22 @@ class AppHomePage extends ConsumerStatefulWidget {
 }
 
 class _AppHomePageState extends ConsumerState<AppHomePage> {
-  bool _animationsStarted = false;
-
   @override
   Widget build(final BuildContext context) {
-    // 1. Sadece video provider'ını izliyoruz
+    final showState = ref.watch(showsProvider(isLimit: false));
     final videoState = ref.watch(homeAssetsProvider);
 
-    if (videoState.isLoading) return const Scaffold(body: ArtisticWebShimmer());
-
-    // 3. HATA DURUMU
-    if (videoState.hasError)
-      return Scaffold(body: _buildSimpleError(context, ref));
-
-    // 4. ANIMASYON TETİKLEYİCİ
-    if (videoState.hasValue &&
-        videoState.value != null &&
-        !_animationsStarted) {
-      Future.delayed(const Duration(milliseconds: 900), () {
-        if (mounted) setState(() => _animationsStarted = true);
-      });
-    }
+    // Eğer veri çekme işlemi tamamen çöktüyse (Network hatası vb.)
+    if (showState.hasError)
+      return GlobalErrorWidget(
+        message: "Sunucuya bağlanılamadı.",
+        onRetry: () => ref.invalidate(showsProvider),
+      );
 
     return Scaffold(
-        body: WebBar(
-            key: NavigationKeys.webBarKey,
-            startAnimations: _animationsStarted));
+      body: WebBar(
+          key: NavigationKeys.webBarKey,
+          startAnimations: widget.startAnimations),
+    );
   }
-
-  Widget _buildSimpleError(final BuildContext context, final WidgetRef ref) =>
-      Center(
-        child: Column(
-          mainAxisAlignment: MainAxisAlignment.center,
-          children: [
-            const Icon(Icons.error_outline, color: Colors.red, size: 60),
-            const SizedBox(height: 16),
-            const Text("Video yüklenirken bir hata oluştu"),
-            TextButton(
-                onPressed: () => ref.invalidate(homeAssetsProvider),
-                child: const Text("Tekrar Dene")),
-          ],
-        ),
-      );
 }
